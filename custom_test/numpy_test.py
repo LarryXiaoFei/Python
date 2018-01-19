aa5cf38bd3c141fe0a2c1f0f1d7c460e3fc05fb4
WalterXue <walterxue@163.com>
2018-01-19 11:52:49 +0800

    numpy数组操作

--- custom_test/numpy_test.py
+++ custom_test/numpy_test.py
@@ -37,10 +37,130 @@ def numpy_func():
     print(B)
 
 
+# N维数组
+def ndarray_func():
+    # 创建一个2X3的二维数组
+    x = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
+    print("np.array：",x)
+    # 输出 二维数组的类型
+    print("type(x)：",type(x))
+    # 输出数组的shape
+    print("x.shape：",x.shape)
+    # 输出数组的类型
+    print("x.dtype：",x.dtype)
+    # The element of x in the *second* row, *third* column, namely, 6.
+    #       0  1   2
+    #  0    1  2   3
+    #  1    4  5   6
+    # x[1, 2]
+    print("x[1, 2]：",x[1, 2])
 
+    # 数组切片 切片可以生成数组的视图
+    y = x[:,1]
+    # y输出为：[2 5]
+    print("y：",y)
+    print("y[0]：",y[0])
+
+
+# 构造数组
+def constructor_array_func():
+    # 构造一个2X2的二维数组
+    y = np.ndarray(shape=(2,2), dtype=float, order='F')
+    print("y：",y)
+    # 构造一个2X2的二维数组
+    x = np.array([[1,2],[3,4]])
+    print("x：",x)
+    # 二维数组转置操作
+    x_T = x.T
+    print("x.T",x_T)
+    # x_T.dtype 获取当前变量类型
+    print("x_T.dtype",x_T.dtype)
+    # x.flags 内存信息
+    print("x.flags",x.flags)
+    x = np.arange(1, 7).reshape(2, 3)
+    print("x",x)
+    # x.flat[3]
+    print("x.flat[3]",x.flat[3])
+    # 数组的虚部
+    x = np.sqrt([1+0j, 0+1j])
+    print("x",x)
+    # image 数组的虚部
+    print("x.imag",x.imag)
+    # real 数组的实部
+    print("x.real",x.real)
+    # 数组中的元素数
+    x = np.zeros((3, 5, 2), dtype=np.complex128)
+    print("x",x)
+    #size 数组中的元素个数
+    print("x.size",x.size)
+    # 元素相乘prod
+    print("xnp.prod(x.shape)",np.prod(x.shape))
+    # itemsize 一个数组元素的长度（以字节为单位）。
+    x = np.array([1,2,3], dtype=np.float64)
+    print("x",x.itemsize)
+    # 复数
+    x = np.array([1,2,3], dtype=np.complex128)
+    print("x",x.itemsize)
+    #x.nbytes 数组的元素消耗的总字节数。
+    print("x.nbytes",x.nbytes)
+    # 数组尺寸数。
+    x = np.array([1, 2, 3])
+    print("x.ndim",x.ndim)
+    #numpy.ndarray.shape 数组维数组。
+    print("x.shape",x.shape)
+    # numpy.ndarray.base
+    x = np.array([1,2,3,4])
+    print("x.base",x.base)
+    y = x[2:]
+    print("y.base",y.base)
+
+
+# 排序、搜索、计数
+def sort_func():
+    x = np.array([3, 1, 2])
+    print("x ",x)
+    # 数组排序
+    print("np.sort(a) ",np.sort(x))
+    # 多维数组排序
+    a = np.array([[1,5,4],[3,2,1]])
+    print("x ",a)
+    # 数组排序 sort along the last axis
+    print("np.sort(a) ",np.sort(a))
+    # sort the flattened array
+    print(" np.sort(a, axis=None) ", np.sort(a, axis=None))
+    #  sort along the first axis
+    print(" np.sort(a, axis=0) ", np.sort(a, axis=0))
+    # 使用键序列执行间接排序。
+    surnames =    ('Hertz',    'Galilei', 'Hertz')
+    first_names = ('Heinrich', 'Galileo', 'Gustav')
+    ind = np.lexsort((first_names, surnames))
+    print("ind",ind)
+    rs = [surnames[i] + ", " + first_names[i] for i in ind]
+    print("rs",rs)
+
+    # 返回将数组分类的索引 返回索引
+    x = np.array([3, 1, 2])
+    print("np.argsort(x)",np.argsort(x))
+    # 排序后重新赋值到新的数组中
+    rs = [x[i] for i in ind]
+    print("rs",rs)
+
+    # 返回沿第一个轴排序的数组的副本。
+    x = np.array([3, 1, 2])
+    print("np.msort(x)",np.msort(x))
+    y = np.sort_complex([5, 3, 6, 2, 1])
+    print("y",y)
+
+    # numpy.argmax
+    a = np.arange(6).reshape(2,3)
+    print("a",a)
+    print("np.argmax(a)",np.argmax(a))
 
 #main函数
 if __name__=="__main__":
     print("开始")
-    numpy_func()
+    # numpy_func()
+    # ndarray_func()
+    # constructor_array_func()
+    sort_func()
     print("结束")
