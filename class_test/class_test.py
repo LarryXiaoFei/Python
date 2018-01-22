9714170c6e2d0cd7ea72e83b641bd21080460fc1
WalterXue <walterxue@163.com>
2018-01-22 15:42:45 +0800

    Python obj\json\dict 之间互相转换

--- class_test/class_test.py
+++ class_test/class_test.py
@@ -4,9 +4,41 @@
 @author:Walter
 """
 from trunk.class_test.employee import Employee
+import json
 
 
 
+"""
+@desc:dict转成obj
+@date:2018-01-22
+@author:Walter
+"""
+def obj_dic(d):
+    top = type('new', (object,), d)
+    seqs = tuple, list, set, frozenset
+    for i, j in d.items():
+        if isinstance(j, dict):
+            setattr(top, i, obj_dic(j))
+        elif isinstance(j, seqs):
+            setattr(top, i,
+                    type(j)(obj_dic(sj) if isinstance(sj, dict) else sj for sj in j))
+        else:
+            setattr(top, i, j)
+    return top
+
+
+"""
+@desc:对象转字典
+@date:2018-01-22
+@author:Walter
+"""
+def props(obj):
+    pr = {}
+    for name in dir(obj):
+        value = getattr(obj, name)
+        if not name.startswith('__') and not callable(value):
+            pr[name] = value
+    return pr
 
 
 """
@@ -14,9 +46,22 @@ from trunk.class_test.employee import Employee
 """
 if __name__=="__main__":
     print("开始")
+    # b={'a': 1, 'b': {'c': 2}, 'd': ['hi', {'foo': 'bar'}]}
+    # x = obj_dic(b)
+    # print(x.a,x.b.c)
+    # y=x.d
+    # t = y[1]
+    # print(t.foo)
+
+
+
+    # employ = json.loads('{"name":"rumor","salary":25}',Employee.__class__)
     employ = Employee("张三",2000)
+    emp=props(employ)
+    print(emp)
     # employ.dispalyEmployee()
-    print("%d" % employ.empCount)
-    employ.displayAge()
+    # print("%d" % employ.empCount)
+    # employ.displayAge()
+
 
 
